5761ea24a844598c82240eb2362f4c11f2578534
Priya Khadke <priyakhadke15@gmail.com>
2020-04-19 18:29:16 -0700

    added toggle switch

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -7,10 +7,33 @@ import './Signup.css'
 
 
 class Signup extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHealthCareProvider: false
+        };
+    }
+
+    onClickHealthCareProviderCheckbox() {
+        this.setState({
+            isHealthCareProvider: !this.state.isHealthCareProvider
+        });
+    }
+
     render() {
         return (
             <Container className="parlex-back-signup"  >
                 <h2 ><FontAwesomeIcon icon={faHeartbeat} size="1x" />  Create Account</h2>
+                <Row style={{ width: "20%", margin: "0 auto", marginTop: "2vh", marginBottom: "2vh" }}>
+                    <Form.Check
+                        type="switch"
+                        id="custom-switch"
+                        label="Health Care Provider?"
+                        checked={this.state.isHealthCareProvider}
+                        onClick={this.onClickHealthCareProviderCheckbox.bind(this)}
+                    />
+                </Row>
                 <Form >
                     <Row>
                         <Form.Group style={{ width: "25%" }} controlId="formGroupFname" >
@@ -28,7 +51,7 @@ class Signup extends Component {
                             <Form.Control type="number" min="1" max="150" placeholder="Select Age" required />
                         </Form.Group>
                         <Form.Group style={{ width: "25%" }} controlId="formGroupCity" >
-                            <Form.Control type="text"  placeholder="Enter City" required />
+                            <Form.Control type="text" placeholder="Enter City" required />
                         </Form.Group>
                         <Form.Group style={{ width: "25%" }} controlId="formGroupZip" >
                             <Form.Control type="text" inputmode="numeric" pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" placeholder="Enter Zip" required />

013545885382bf04ea45d7aa405606e0bef4833d
deepikay912 <deepika640yannamani@gmail.com>
2020-04-19 20:03:38 -0700

    be port 3001

--- Backend/config.js
+++ Backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     MONGODB_URL : 'mongodb+srv://kartiar:Shallow123@edgehealth-gi1gw.mongodb.net/EdgeHealthDB?retryWrites=true&w=majority',
     JWT_KEY : 'WinterIsComingGOT2019',
-    PORT : '3000'
+    PORT : '3001'
 };

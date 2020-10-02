a4b3e810629cfebe5b77280a8223356d57109304
Priya Khadke <priyakhadke15@gmail.com>
2020-10-01 17:50:11 -0700

    Formatted Patient profile page

--- frontend/src/components/Profile/PatientProfile.js
+++ frontend/src/components/Profile/PatientProfile.js
@@ -127,121 +127,100 @@ class PatientProfile extends Component {
             <Container className="parlex-back-signup">
                 {this.state.isLogin === false ? <Redirect to="/login" /> : ""}
                 <h2><FontAwesomeIcon icon={faHeartbeat} size="1x" />  Patient Profile</h2>
-
-                <Container style={{ width: "80%", margin: "0 auto" }}>
+                <Container style={{ width: "85%", margin: "0 auto" }}>
                     <Form onSubmit={this.onSubmit.bind(this)}>
-                        <Row className="signup-row">
-                            <Col md={6}>
-                                <Form.Label >firstname</Form.Label>
-                                <Form.Control name="firstname" type="text" defaultValue={this.state.firstName} autoFocus />
-                            </Col>
-                            <Col md={6}>
-                                <Form.Label >lastname</Form.Label>
-                                <Form.Control name="lastname" type="text" defaultValue={this.state.lastName} />
-                            </Col>
-                        </Row>
-                        <Row className="signup-row">
-                            <Col md={6}>
-                                <Form.Label >Email ID</Form.Label>
-                                <Form.Control name="email" type="email" defaultValue={this.state.emailId} readOnly />
-                            </Col>
-                            <Col md={6}>
-                                <Form.Label >Password</Form.Label>
-                                <Form.Control name="password" placeholder="<unchanged>" type="password" />
-                            </Col>
-                        </Row>
-                        <Row className="signup-row">
-                            <Col md={5}>
-                                <Form.Label >address</Form.Label>
-                                <Form.Control name="address" type="text" defaultValue={this.state.address} />
-                            </Col>
-                            <Col md={3}>
-                                <Form.Label >city</Form.Label>
-                                <Form.Control name="city" type="text" defaultValue={this.state.city} />
-                            </Col>
-                            <Col md={2}>
-                                <Form.Label >State</Form.Label>
-                                <Form.Control name="state" value={this.state.state} as="select" >
-                                    <option>AL</option>
-                                    <option>AK</option>
-                                    <option>AZ</option>
-                                    <option>AR</option>
-                                    <option>CA</option>
-                                    <option>CO</option>
-                                    <option>CT</option>
-                                    <option>DE</option>
-                                    <option>DC</option>
-                                    <option>FL</option>
-                                    <option>GA</option>
-                                    <option>HI</option>
-                                    <option>ID</option>
-                                    <option>IL</option>
-                                    <option>IN</option>
-                                    <option>IA</option>
-                                    <option>KS</option>
-                                    <option>KY</option>
-                                    <option>LA</option>
-                                    <option>ME</option>
-                                    <option>MD</option>
-                                    <option>MA</option>
-                                    <option>MI</option>
-                                    <option>MN</option>
-                                    <option>MS</option>
-                                    <option>MO</option>
-                                    <option>MT</option>
-                                    <option>NE</option>
-                                    <option>NV</option>
-                                    <option>NH</option>
-                                    <option>NJ</option>
-                                    <option>NM</option>
-                                    <option>NY</option>
-                                    <option>NC</option>
-                                    <option>ND</option>
-                                    <option>OH</option>
-                                    <option>OK</option>
-                                    <option>OR</option>
-                                    <option>PA</option>
-                                    <option>RI</option>
-                                    <option>SC</option>
-                                    <option>SD</option>
-                                    <option>TN</option>
-                                    <option>TX</option>
-                                    <option>UT</option>
-                                    <option>VT</option>
-                                    <option>VA</option>
-                                    <option>WA</option>
-                                    <option>WV</option>
-                                    <option>WI</option>
-                                    <option>WY</option>
-                                </Form.Control>
-                            </Col>
-                            <Col md={2}>
-                                <Form.Label >zipcode</Form.Label>
-                                <Form.Control name="zipcode" inputMode="numeric" pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" defaultValue={this.state.zip} />
-                            </Col>
-                        </Row>
-                        <Row className="signup-row">
-                            <Col md={4}>
-                                <Form.Label >phoneNumber</Form.Label>
-                                <Form.Control name="phoneNumber" type="tel" defaultValue={this.state.phone} />
-                            </Col>
-                            <Col md={4}>
-                                <Form.Label >emergencyContact</Form.Label>
-                                <Form.Control name="emergencyContact" type="tel" defaultValue={this.state.emergencyContact} />
-                            </Col>
-                            <Col md={2}>
-                                <Form.Label >gender</Form.Label>
-                                <Form.Control name="gender" value={this.state.gender} as="select">
-                                    <option>Male</option>
-                                    <option selected>Female</option>
-                                    <option>Other</option>
-                                </Form.Control>
-                            </Col>
-                            <Col md={2}>
-                                <Form.Label >age</Form.Label>
-                                <Form.Control name="age" type="number" min="0" max="150" defaultValue={this.state.age} />
-                            </Col>
-                        </Row>
+                        <Form.Group as={Row} className="signup-row" controlId="nameid">
+                            <Form.Label column md="2">First Name</Form.Label>
+                            <Col md={4}><Form.Control name="firstname" type="text" defaultValue={this.state.firstName} autoFocus /></Col>
+                            <Form.Label column md="2">Last Name</Form.Label>
+                            <Col md={4}><Form.Control name="lastname" type="text" defaultValue={this.state.lastName} /></Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="signup-row" controlId="emailid">
+                            <Form.Label column md="2">Email ID</Form.Label>
+                            <Col md={4}><Form.Control name="email" type="email" defaultValue={this.state.emailId} readOnly /></Col>
+                            <Form.Label column md="2">Password</Form.Label>
+                            <Col md={4}><Form.Control name="password" placeholder="<unchanged>" type="password" /></Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="signup-row" controlId="addressid">
+                            <Form.Label column md="2">Address</Form.Label>
+                            <Col md={4}><Form.Control name="address" type="text" defaultValue={this.state.address} /></Col>
+                            <Form.Label column md="2">City</Form.Label>
+                            <Col md={4}> <Form.Control name="city" type="text" defaultValue={this.state.city} /></Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="signup-row" controlId="stateid">
+                            <Form.Label column md="2">State</Form.Label>
+                            <Col md={4}> <Form.Control name="state" value={this.state.state} as="select" >
+                                <option>AL</option>
+                                <option>AK</option>
+                                <option>AZ</option>
+                                <option>AR</option>
+                                <option>CA</option>
+                                <option>CO</option>
+                                <option>CT</option>
+                                <option>DE</option>
+                                <option>DC</option>
+                                <option>FL</option>
+                                <option>GA</option>
+                                <option>HI</option>
+                                <option>ID</option>
+                                <option>IL</option>
+                                <option>IN</option>
+                                <option>IA</option>
+                                <option>KS</option>
+                                <option>KY</option>
+                                <option>LA</option>
+                                <option>ME</option>
+                                <option>MD</option>
+                                <option>MA</option>
+                                <option>MI</option>
+                                <option>MN</option>
+                                <option>MS</option>
+                                <option>MO</option>
+                                <option>MT</option>
+                                <option>NE</option>
+                                <option>NV</option>
+                                <option>NH</option>
+                                <option>NJ</option>
+                                <option>NM</option>
+                                <option>NY</option>
+                                <option>NC</option>
+                                <option>ND</option>
+                                <option>OH</option>
+                                <option>OK</option>
+                                <option>OR</option>
+                                <option>PA</option>
+                                <option>RI</option>
+                                <option>SC</option>
+                                <option>SD</option>
+                                <option>TN</option>
+                                <option>TX</option>
+                                <option>UT</option>
+                                <option>VT</option>
+                                <option>VA</option>
+                                <option>WA</option>
+                                <option>WV</option>
+                                <option>WI</option>
+                                <option>WY</option>
+                            </Form.Control></Col>
+                            <Form.Label column md="2">Zip Code</Form.Label>
+                            <Col md={4}> <Form.Control name="zipcode" inputMode="numeric" pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" defaultValue={this.state.zip} /></Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="signup-row" controlId="contactid">
+                            <Form.Label column md="2">Phone Number</Form.Label>
+                            <Col md={4}> <Form.Control name="phoneNumber" type="tel" defaultValue={this.state.phone} /></Col>
+                            <Form.Label column md="2">Emergency Contact</Form.Label>
+                            <Col md={4}> <Form.Control name="emergencyContact" type="tel" defaultValue={this.state.emergencyContact} /></Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="signup-row" controlId="genderid">
+                            <Form.Label column md="2" >Gender</Form.Label>
+                            <Col md={4}><Form.Control name="gender" value={this.state.gender} as="select">
+                                <option>Male</option>
+                                <option selected>Female</option>
+                                <option>Other</option>
+                            </Form.Control></Col>
+                            <Form.Label column md="2">Age</Form.Label>
+                            <Col md={4}><Form.Control name="age" type="number" min="0" max="150" defaultValue={this.state.age} /></Col>
+                        </Form.Group>
                         <Row style={{ width: "20%", margin: "0 auto", marginTop: "2vh", marginBottom: "2vh" }}>
                             <Form.Check
                                 type="switch"
@@ -255,11 +234,11 @@ class PatientProfile extends Component {
                             (<Row>
                                 <Col md={3}></Col>
                                 <Col md={3}>
-                                    <Form.Label >cigsperday</Form.Label>
+                                    <Form.Label >Cigarates per day</Form.Label>
                                     <Form.Control name="cigsperday" type="number" min="1" max="50" defaultValue="Cigarates per day" />
                                 </Col>
                                 <Col md={3}>
-                                    <Form.Label >smokeyears</Form.Label>
+                                    <Form.Label >Smoking age in years</Form.Label>
                                     <Form.Control name="smokeyears" type="number" min="1" max="50" defaultValue="Smoking years" />
                                 </Col>
                                 <Col md={3}></Col>

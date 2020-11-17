37c572bae58bc73507bdbfaf652a0d0558c94a1e
Shivani Reddy Singireddy <shivanireddysingireddy@Shivanis-MacBook-Pro.local>
2020-11-17 15:35:01 -0800

    Modified schema for simulated data

--- Backend/schema/SimulatedData.js
+++ Backend/schema/SimulatedData.js
@@ -27,28 +27,281 @@ const simulatedDataSchema = mongoose.Schema({
     required: false
   },
 
-  cholesterol : {
-    type: Number,
-    required: false
-  },
+  // cholesterol : {
+  //   type: Number,
+  //   required: false
+  // },
+  
   bloodPressure : {
     type: Number,
     required: false
   },
 
-  fbs : {
-    type: Boolean,
-    required: false 
-  },
-  exang : {
-    type: Boolean,
-    required: false 
-  },
+  // fbs : {
+  //   type: Boolean,
+  //   required: false 
+  // },
+  // exang : {
+  //   type: Boolean,
+  //   required: false 
+  // },
 
   heartRate : {
     type: Number,
     required: false
+  },
+
+  //-----
+  Painloc : {
+    type: Number,
+   required: false
+  },
+
+  Painexer : {
+    type: Number,
+    required: false
+  },
+
+  htn : {
+    type: Number,
+   required: false
+  },
+
+  Famhist : {
+    type: Number,
+    required: false
+  },
+
+  Dig : {
+    type: Number,
+   required: false
+  },
+
+  prop : {
+    type: Number,
+    required: false
+  },
+
+  nitr : {
+    type: Number,
+   required: false
+  },
+
+  Pro : {
+    type: Number,
+    required: false
+  },
+
+  Chol : {
+    type: Number,
+   required: false
+  },
+
+  Fbs : {
+    type: Number,
+    required: false
+  },
+
+  Thalach : {
+    type: Number,
+   required: false
+  },
+
+  Thalrest : {
+    type: Number,
+    required: false
+  },
+
+
+
+  Tpeakbps : {
+    type: Number,
+    required: false
+  },
+
+  Tpeakbpd : {
+    type: Number,
+   required: false
+  },
+
+  Trestbpd : {
+    type: Number,
+    required: false
+  },
+
+  Thaldur : {
+    type: Number,
+   required: false
+  },
+
+  Met : {
+    type: Number,
+    required: false
+  },
+
+  exang : {
+    type: Number,
+    required: false
+  },
+
+  Xhypo : {
+    type: Number,
+   required: false
+  },
+
+  Old_peak : {
+    type: Number,
+    required: false
+  },
+
+  Slope : {
+    type: Number,
+   required: false
+  },
+
+  Rldv5 : {
+    type: Number,
+    required: false
+  },
+
+
+
+
+  Rldv5e : {
+    type: Number,
+    required: false
+  },
+
+  Cp_encoded : {
+    type: Number,
+   required: false
+  },
+
+  Restcg_encoded : {
+    type: Number,
+    required: false
+  },
+
+  cp_1 : {
+    type: Number,
+   required: false
+  },
+
+  cp_2 : {
+    type: Number,
+    required: false
+  },
+
+  cp_3 : {
+    type: Number,
+    required: false
+  },
+
+  cp_4 : {
+    type: Number,
+   required: false
+  },
+
+  relrest : {
+    type: Number,
+    required: false
+  },
+
+ 
+
+  relrest : {
+    type: Number,
+    required: false
+  },
+
+  restecg_0 : {
+    type: Number,
+   required: false
+  },
+
+  restecg_1 : {
+    type: Number,
+    required: false
+  },
+
+  restecg_2 : {
+    type: Number,
+    required: false
+  },
+
+  Age : {
+    type: Number,
+   required: false
+  },
+
+  Sex : {
+    type: Number,
+    required: false
+  },
+
+  PatientId : {
+    type: String,
+   required: false
+  },
+
+  Trestbps : {
+    type: Number,
+    required: false
+  },
+
+  risk_level :{
+    type: String,
+    required: false
+  },
+
+  risk_factor :{
+    type: Number,
+   required: false
   }
+
+  // int Painloc;
+	// int Painexer;
+	// int htn;
+	// int Famhist; 
+	// int Dig;
+	// int prop; 
+	// int nitr; 
+	// int Pro;
+	// int Diuretic; 
+	
+	// int Chol;
+	// int Fbs;
+	// int Thalach;
+	// int Thalrest; 
+	// int Tpeakbps ;
+	// int Tpeakbpd;
+	// int Trestbpd ;
+	// int Thaldur; 
+	// int Met;
+	// int exang;
+	// int Xhypo; 
+	// int Old_peak;
+	// int Slope; 
+	// int Rldv5;
+	// int Rldv5e; 
+	// int Cp_encoded;
+	// int Restcg_encoded;
+  // int cp_1;
+	// int cp_2;
+	// int cp_3;
+	// int cp_4;
+	// int relrest;
+	// int restecg_0;
+	// int restecg_1;
+  // int restecg_2;
+  // int Age;
+	// int Sex;
+	
+	
+	// String PatientId;
+	
+	// /* G Values */
+	// int Trestbps;
 
 });
 
